afa8af7f8edf253950e4d46df47b15170bafe903
Evgeniy <evsinev@gmail.com>
2011-07-20 18:25:55 +0300

    Added CreateWikiPages

---
+++ src/test/java/wiki_trac2redmine/CreateWikiPagesTest.java
@@ -0,0 +1,11 @@
+package wiki_trac2redmine;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: esinev
+ * Date: 7/20/11
+ * Time: 4:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CreateWikiPagesTest {
+}
